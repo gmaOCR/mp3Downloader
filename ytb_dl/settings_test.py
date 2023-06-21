8a1794c88eba004117be7e90106b4c6b6301df90
Greg M <marainigregory@pm.me>
2023-06-21 10:17:54 +0200

    correction import

--- ytb_dl/settings_test.py
+++ ytb_dl/settings_test.py
@@ -1,15 +1,5 @@
 from settings import *
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'downloader',
-]
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
