021c828541c921eb1a054c7f018ee2f527036151
Greg M <marainigregory@pm.me>
2023-06-23 12:42:12 +0200

    model upragde

--- ytb_dl/downloader/models.py
+++ ytb_dl/downloader/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Video(models.Model):
     link = models.CharField(max_length=255)
     output_format = models.CharField(max_length=20)
+    progress = models.IntegerField(default=0)
